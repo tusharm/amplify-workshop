19cdc9a5fd6394ac9ed859621ac1178f06afbd2f
Tushar Madhukar <tushar.madhukar@gmail.com>
2025-04-25 23:38:17 +1000

    feat: Added Amplify

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,6 +1,10 @@
 'use client'
 import React from 'react';
 import dynamic from 'next/dynamic';
+import { Amplify } from 'aws-amplify';
+import config from '../../../amplify_outputs.json';
+
+Amplify.configure(config);
 
 const App = dynamic(() => import('../../App'), { ssr: false })
 
